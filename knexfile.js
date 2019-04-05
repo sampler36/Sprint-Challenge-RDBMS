6ca786b72de100fce71498decb016201e4f0318b
Talent Antonio <antonio.talenty@gmail.com>
2019-04-05 08:26:54 +0100

    Projects table

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,15 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './projectTracker.sqlite3'
+    },
+    migrations:{
+      directory:'./data/migrations'
+     },
+     seeds:{
+       directory:'./data/seeds'
+     },
+     useNullAsDefault: true
   },
+
 };
